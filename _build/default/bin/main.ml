9591cf0aa4df7a44cbb4bcc2d2ff6ada5723b4dc
AngelVI <a.v.iliev13@gmail.com>
2023-10-14 14:58:11 +0300

    generate espanso matches

--- _build/default/bin/main.ml
+++ _build/default/bin/main.ml
@@ -1,6 +1,10 @@
-type keyword = { name : string; docs : string } (* ; args: string list} *)
+type keyword = { name : string; docs : string; args : string list }
+[@@deriving show]
 
-let find_node_with_tag node search_tag =
+let print_keywords lst =
+  List.map show_keyword lst |> String.concat "\n" |> print_endline
+
+let find_node_with_tag search_tag node =
   List.find
     (function
       | n ->
@@ -8,43 +12,65 @@ let find_node_with_tag node search_tag =
           tag = search_tag)
     (Xml.children node)
 
-let get_node_text node = 
-    let children = Xml.children node in
-    match children with 
-    | [] -> ""
-    | hd::_ -> Xml.pcdata hd
+let get_node_text node =
+  let children = Xml.children node in
+  match children with [] -> "" | hd :: _ -> Xml.pcdata hd
 
-let parse_node node =
-  let name = Xml.attrib node "name" in
-  let docsNode = find_node_with_tag node "doc" in
-
-  print_endline "------------";
-  let docs = get_node_text docsNode in
-  (* {name=name; docs=""; args=[]} *)
-  { name; docs }
-
-let rec get_keywords_aux acc node =
+let rec get_keywords_aux acc node search_tag =
   match node with
   | [] -> acc
   | hd :: tl ->
       let tag = try Xml.tag hd with _ -> "unknown" in
       let acc =
         match tag with
-        | "kw" -> parse_node hd :: acc
+        | t when t = search_tag -> hd :: acc
         | "unknown" -> acc
-        | _ -> acc @ get_keywords_aux [] (Xml.children hd)
+        | _ -> acc @ get_keywords_aux [] (Xml.children hd) search_tag
       in
 
-      get_keywords_aux acc tl
+      get_keywords_aux acc tl search_tag
 
-let get_keywords xml = get_keywords_aux [] @@ Xml.children xml
+let get_keywords xml search_tag =
+  get_keywords_aux [] (Xml.children xml) search_tag
+
+let parse_node node =
+  let name = Xml.attrib node "name" in
+  let docs = find_node_with_tag "doc" node |> get_node_text in
+  let args =
+    get_keywords node "arg"
+    |> List.map @@ find_node_with_tag "name"
+    |> List.map get_node_text
+  in
+  { name; docs; args }
+
+(*
+let matchTemplate = "
+  - trigger: \"%s\"
+    form: \"%s\"
+    ";;
+*)
+
+let rec format_args acc args =
+    match args with 
+    | [] -> String.concat "" acc
+    | hd::tl -> format_args ((Printf.sprintf " %s=[[%s]]" hd hd)::acc) tl
+
+let rec convert_kwds_to_espanso acc kwds = 
+    match kwds with 
+    | [] -> acc
+    | hd::tl -> (
+        let trigger = String.lowercase_ascii hd.name in
+        let form = hd.name in
+        let form = form ^ ( if List.length hd.args > 0 then " |" else "" ) in
+        let form = form ^ (format_args [] hd.args) in
+        let acc = (Printf.sprintf "\n  - trigger: \"%s\"\n    form: \"%s\"\n" trigger form)::acc in
+        convert_kwds_to_espanso acc tl)
+
 
 let () =
   let xml_path = "./keywords.xml" in
   let xml = Xml.parse_file xml_path in
-  let kwds = get_keywords xml in
-  List.iter
-    (function el -> print_endline @@ Printf.sprintf "%s = %s" el.name el.docs)
-    kwds;
-  print_int @@ List.length kwds;
-  print_newline ()
+  let kwds = get_keywords xml "kw" |> List.map parse_node in
+  print_keywords kwds;
+  let out = convert_kwds_to_espanso [] kwds in
+  print_string @@ String.concat "\n" out
--- bin/main.ml
+++ bin/main.ml
@@ -1,6 +1,10 @@
-type keyword = { name : string; docs : string }
+type keyword = { name : string; docs : string; args : string list }
+[@@deriving show]
 
-let find_node_with_tag node search_tag =
+let print_keywords lst =
+  List.map show_keyword lst |> String.concat "\n" |> print_endline
+
+let find_node_with_tag search_tag node =
   List.find
     (function
       | n ->
@@ -12,33 +16,63 @@ let get_node_text node =
   let children = Xml.children node in
   match children with [] -> "" | hd :: _ -> Xml.pcdata hd
 
-let parse_node node =
-  let name = Xml.attrib node "name" in
-  let docs = find_node_with_tag node "doc" |> get_node_text in
-  { name; docs }
-
-let rec get_keywords_aux acc node =
+let rec get_keywords_aux acc node search_tag =
   match node with
   | [] -> acc
   | hd :: tl ->
       let tag = try Xml.tag hd with _ -> "unknown" in
       let acc =
         match tag with
-        | "kw" -> parse_node hd :: acc
+        | t when t = search_tag -> hd :: acc
         | "unknown" -> acc
-        | _ -> acc @ get_keywords_aux [] (Xml.children hd)
+        | _ -> acc @ get_keywords_aux [] (Xml.children hd) search_tag
       in
 
-      get_keywords_aux acc tl
+      get_keywords_aux acc tl search_tag
 
-let get_keywords xml = get_keywords_aux [] @@ Xml.children xml
+let get_keywords xml search_tag =
+  get_keywords_aux [] (Xml.children xml) search_tag
+
+let parse_node node =
+  let name = Xml.attrib node "name" in
+  let docs = find_node_with_tag "doc" node |> get_node_text in
+  let args =
+    get_keywords node "arg"
+    |> List.map @@ find_node_with_tag "name"
+    |> List.map get_node_text
+  in
+  { name; docs; args }
+
+(*
+let matchTemplate = "
+  - trigger: \"%s\"
+    form: \"%s\"
+    ";;
+*)
+
+let rec format_args acc args =
+  match args with
+  | [] -> String.concat "" acc
+  | hd :: tl -> format_args (Printf.sprintf " %s=[[%s]]" hd hd :: acc) tl
+
+let rec convert_kwds_to_espanso_aux acc kwds =
+  match kwds with
+  | [] -> acc
+  | hd :: tl ->
+      let trigger = String.lowercase_ascii hd.name in
+      let form = hd.name in
+      let form = form ^ if List.length hd.args > 0 then " |" else "" in
+      let form = form ^ format_args [] hd.args in
+      (* how to sprintf with a var instead of having to do this in place *)
+      let out =
+        Printf.sprintf "\n  - trigger: \"%s\"\n    form: \"%s\"\n" trigger form
+      in
+      convert_kwds_to_espanso_aux (out :: acc) tl
+
+let convert_kwds_to_espanso kwds = convert_kwds_to_espanso_aux [] kwds
 
 let () =
-  let xml_path = "./keywords.xml" in
-  let xml = Xml.parse_file xml_path in
-  let kwds = get_keywords xml in
-  List.iter
-    (function el -> print_endline @@ Printf.sprintf "%s = %s" el.name el.docs)
-    kwds;
-  print_int @@ List.length kwds;
-  print_newline ()
+  let xml = "./keywords.xml" |> Xml.parse_file in
+  let kwds = get_keywords xml "kw" |> List.map parse_node in
+  let out = convert_kwds_to_espanso kwds in
+  print_string @@ String.concat "\n" out
